9341c1175d089a14093accfd440f1bae79f3748c
madawa917 <madawa917@gmail.com>
2021-05-29 15:01:03 +0530

    HTML pages

--- src/main/java/com/task2_1/demo/controller/AuthenticationController.java
+++ src/main/java/com/task2_1/demo/controller/AuthenticationController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.task2_1.demo.model.User;
+import com.task2_1.demo1.model.User;
 
 @Controller
 public class AuthenticationController {
